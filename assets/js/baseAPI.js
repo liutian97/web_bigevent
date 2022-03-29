fb503a908b666f5f64e313fa1e4782803966793f
liutian97 <liutian9707@163.com>
2022-03-29 08:44:15 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,23 @@
 // 每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，会先调用 $.ajaxPrefilter() 这个函数。 拿到给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+  // 统一为有权限的接口设置headers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      // 1. 强制清空 token
+      localStorage.removeItem('token');
+      // 2. 强制跳转到登录页面
+      location.href = '/login.html';
+    }
+  }
+
+
 })
